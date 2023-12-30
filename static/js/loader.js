83b12de3995ab4cc882e32731dda635a1b6ed6f1
xenotation <63644334+xenotation@users.noreply.github.com>
2023-12-30 13:13:50 +0200

    stoped the blur loader WAS STOPPING THE LIGHTBOX FROM CENTERING

--- static/js/loader.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Wait for the content to load, then remove the blur
-    document.querySelector("body").style.filter = "blur(0)";
-  });
-  
