f4db7c67afb3e9c0a1d6b879378df396b7311514
xenotation <63644334+xenotation@users.noreply.github.com>
2023-12-30 11:26:06 +0200

    remvoed glightbox

---
+++ public/js/loader.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Wait for the content to load, then remove the blur
+    document.querySelector("body").style.filter = "blur(0)";
+  });
+  
