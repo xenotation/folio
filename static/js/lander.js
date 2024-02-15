e02fca3933becae7a58d139aaa31296116780fe9
xenotation <63644334+xenotation@users.noreply.github.com>
2024-02-15 12:31:50 +0200

    alternative images for llander on phone

--- static/js/lander.js
+++ static/js/lander.js
@@ -1,37 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-    function playRandomVideo(videoElement, videoFiles) {
-        // Get a random video filename
-        var randomVideo = videoFiles[Math.floor(Math.random() * videoFiles.length)];
-
-        // Set the new video source
-        videoElement.src = '/images/' + randomVideo;
-
-        // Play the video
-        videoElement.play();
+    function playRandomVideoOrImage(videoContainer, videoFiles, imageFiles) {
+        if (isMobileDevice()) {
+            // If user is on a mobile device, display a random image
+            var randomImage = imageFiles[Math.floor(Math.random() * imageFiles.length)];
+            var img = document.createElement('img');
+            img.src = '/images/' + randomImage;
+            img.alt = 'Random Image';
+            img.classList.add('video-container');
+            videoContainer.innerHTML = '';
+            videoContainer.appendChild(img);
+        } else {
+            // If not on a mobile device, play a random video
+            var randomVideo = videoFiles[Math.floor(Math.random() * videoFiles.length)];
+            var videoElement = document.createElement('video');
+            videoElement.src = '/images/' + randomVideo;
+            videoElement.autoplay = true;
+            videoElement.muted = true;
+            videoElement.classList.add('video-container');
+            videoContainer.innerHTML = '';
+            videoContainer.appendChild(videoElement);
+            videoElement.play();
+        }
     }
 
-    // Get all elements with the 'video-container' class
+    function isMobileDevice() {
+        return /Mobi|Android/i.test(navigator.userAgent);
+    }
+
     var videoContainers = document.getElementsByClassName('video-container');
-
-    // List of video filenames in the 'videos' folder
     var videoFiles = ['Turing-Pattern-1.webm', 'Turing-Pattern-2.webm', 'Turing-Pattern-3.webm', 'Turing-Pattern-4.webm', 'Turing-Pattern-5.webm', 'Turing-Pattern-6.webm'];
+    var imageFiles = ['Turing-Pattern-1.webp'];
 
-    // Loop through each container
     Array.from(videoContainers).forEach(function (videoContainer) {
-        // Create a video element and set its attributes
-        var videoElement = document.createElement('video');
-        videoElement.autoplay = true;
-        videoElement.muted = true;
+        playRandomVideoOrImage(videoContainer, videoFiles, imageFiles);
 
-        // Append the video element to the container
-        videoContainer.appendChild(videoElement);
-
-        // Play the initial random video
-        playRandomVideo(videoElement, videoFiles);
-
-        // Listen for the 'ended' event to play another random video
-        videoElement.addEventListener('ended', function () {
-            playRandomVideo(videoElement, videoFiles);
+        videoContainer.addEventListener('click', function () {
+            playRandomVideoOrImage(videoContainer, videoFiles, imageFiles);
         });
     });
 });
